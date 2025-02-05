cfbc777c064c70889252eedcc1539dc5de835b16
Tejassingh29 <tejas.singh@kalvium.community>
2025-02-05 11:56:42 +0530

    mongoosedb added

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const connectdataBse = require("./dataBase");
+// database()
 const app = express();
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack); // Log the error for debugging
     res.status(500).json({ error: "Internal Server Error" });
 });
+connectdataBse();
 
 // Basic /ping route with error handling
 app.get("/ping", (req, res, next) => {
