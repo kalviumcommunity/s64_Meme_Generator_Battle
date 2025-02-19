be54267b46c7619c32b2cec0ed7ffeed7a58c57d
Tejassingh29 <tejas.singh@kalvium.community>
2025-02-19 12:09:28 +0530

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const Database = {}
 
 const app = express();
 const PORT = process.env.PORT || 8000;
